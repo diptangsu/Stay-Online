a33934db9bb9305e41bcf65edf22e23be0417c4b
Diptangsu Goswami <deep.dippu@gmail.com>
2021-06-16 19:47:18 +0530

    Add files via upload

---
+++ stay_online.py
@@ -0,0 +1,28 @@
+import pyautogui
+from time import perf_counter
+
+MOVE_AMOUNT = 10  # px
+SLEEP_THRESHOLD = 3  # seconds
+
+def move_mouse():
+    pyautogui.moveRel(-MOVE_AMOUNT, 0)
+    pyautogui.moveRel(0, -MOVE_AMOUNT)
+    pyautogui.moveRel(MOVE_AMOUNT, 0)
+    pyautogui.moveRel(0, MOVE_AMOUNT)
+
+x_, y_ = pyautogui.position()
+start = perf_counter()
+
+while True:
+    x, y = pyautogui.position()
+    pos = f'X: {x:<5} Y: {y:<5}'
+    print(pos, end='')
+    print('\b' * len(pos), end='', flush=True)
+
+    now = perf_counter()
+    # ASSUMPTION: human mouse use, movement > 10 px
+    if abs(x - x_) > MOVE_AMOUNT or abs(y - y_) > MOVE_AMOUNT:
+        x_, y_ = x, y
+        start = now
+    elif now - start >= 3:
+        move_mouse()
